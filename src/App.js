af13abf233c1902778b427970306e072a3bac212
Mayon Francis <mayonfrancis@gmail.com>
2022-01-29 23:07:59 +0530

    merge

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           <Route exact path="/fortrain" element={<><Navbar user={user} setUser={setUser} /><TrainHome seat={seat} setSeat={setSeat} /></>} />
           <Route exact path="/cart" element={<><Navbar user={user} setUser={setUser}/><Cart seat={seat} /></>} />
           <Route exact path="/trainroute" element={<><Navbar /><TrainRoute station={station} setStation={setStation} /></>} />
-
+          <Route exact path="/order" element={<><Navbar station={station} user={user} setUser={setUser} /><Dine /></>} />
         </Routes>
 
 
