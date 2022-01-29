68b436c213fa645c44b04d88bdbbbeb8e4c8853d
Mayon Francis <mayonfrancis@gmail.com>
2022-01-29 22:45:52 +0530

    Persist user

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ import Dine from './components/Dine/dine';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import {useState} from 'react';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route exact path="/" element={<><Navbar /><Home /></>} />
-          <Route exact path="/fortrain" element={<><Navbar /><TrainHome /></>} />
-          <Route exact path="/order" element={<><Navbar /><Dine /></>} />
+          <Route exact path="/" element={<><Navbar user={user} setUser={setUser} /><Home /></>} />
+          <Route exact path="/fortrain" element={<><Navbar user={user} setUser={setUser} /><TrainHome /></>} />
+          <Route exact path="/order" element={<><Navbar user={user} setUser={setUser} /><Dine /></>} />
 
         </Routes>
 
