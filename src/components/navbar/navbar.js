946a8e801eca7ced776b13a07ef560a02b04faaa
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-01-29 14:33:50 +0530

    logo

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './navbar.css'
 import Signup from '../Modals/Signup/Signup';
 import Login from '../Modals/Login/Login';
+import logo from '../../assets/images/logo.png'
 
 function Navbar() {
     return <div className='navbar'>
         <div className="nav__left">
+            <img src={logo} alt="" />
             <p>Trivandrum</p>
         </div>
         <div className="nav__right">
             <ul>
                 <li>Home</li>
-                <li><Login/></li>
-                <li><Signup/></li>
+                <li><Login /></li>
+                <li><Signup /></li>
                 <li>Contact</li>
             </ul>
         </div>
