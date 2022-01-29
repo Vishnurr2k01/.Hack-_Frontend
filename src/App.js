7c314da22c677b30eb86ace03d28f39a51c99da2
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-01-29 15:06:34 +0530

    router

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import Navbar from './components/navbar/navbar';
 import TrainHome from './components/trainhome/trainhome';
 import Signup from './components/Signup/Signup';
 import Dine from './components/Dine/dine';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      {/* <Signup/> */}
-      {/* <Home /> */}
-      {/* <TrainHome /> */}
-      <Dine />
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<><Navbar /><Home /></>} />
+          <Route exact path="/fortrain" element={<><Navbar /><TrainHome /></>} />
+          <Route exact path="/order" element={<><Navbar /><Dine /></>} />
+
+        </Routes>
+
+        {/* <Navbar /> */}
+        {/* <Signup/> */}
+        {/* <Home /> */}
+        {/* <TrainHome /> */}
+        {/* <Dine /> */}
+
+      </div>
+    </BrowserRouter>
+  )
 }
+
 
 export default App;
